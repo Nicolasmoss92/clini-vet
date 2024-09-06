d5508639f786c690c1b9d6efb79a1cffc2fc15b4
00043963 <00043963@randon.com.br>
2024-09-05 21:57:33 -0300

    feat: footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from '../styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <p>Contato: (11) 1234-5678 | email@qualcnpj.com.br</p>
+      </div>
+      <div>
+        <p>&copy; 2024 QualCNPJ. Todos os direitos reservados.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
