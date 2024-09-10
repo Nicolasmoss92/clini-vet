51dc8323810fa698ef765bb40d824deb35898549
00043963 <00043963@randon.com.br>
2024-09-10 11:00:30 -0300

    feat: page on duty

--- src/components/layout/onDuty/header.tsx
+++ src/components/layout/onDuty/header.tsx
@@ -3,7 +3,7 @@
 export default function SubHeader() {
   return (
     <header className="bg-green-500 text-white w-full h-16 flex items-center justify-center">
-      <div className="text-white text-lg font-semibold">AMO PET SISTER</div>
+      <div className="text-white text-lg font-semibold">PLANTÕES</div>
     </header>
   );
 }
