3692c185fe42e1f650737b393fe9f4573eba523a
00043963 <00043963@randon.com.br>
2024-09-16 16:27:09 -0300

    feat: padronização subheader

--- src/components/layout/surgery/SubHeader.tsx
+++ src/components/layout/surgery/SubHeader.tsx
@@ -3,7 +3,7 @@
 export default function SubHeader() {
   return (
     <header className="bg-green-500 text-white w-full h-16 flex items-center justify-center">
-      <div className="text-white text-lg font-semibold">CIRURGIAS</div>
+      <div className="text-white text-lg font-semibold">Cirurgias</div>
     </header>
   );
 }
